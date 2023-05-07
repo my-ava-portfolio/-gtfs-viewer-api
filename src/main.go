bce8422a8f406605c607c9a8644d10afe7ca7d31
amauryval <amauryval@gmail.com>
2023-05-07 19:42:13 +0200

    upd (main) CORS

--- src/main.go
+++ src/main.go
@@ -11,7 +11,9 @@ func setupRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:4200", "https://portfolio.amaury-valorge.com"},
+		AllowOrigins:     []string{
+		    "http://localhost:4200", "https://portfolio.amaury-valorge.com", "https://preprod.amaury-valorge.com"
+		},
 		AllowMethods:     []string{"GET"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
